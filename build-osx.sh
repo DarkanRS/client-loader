22157fd8546fa4c118984d3678a927afb44fc59b
Pete Kemp <pkemp1992@live.co.uk>
2022-11-19 22:28:31 +0000

    Proper info.plist and move packr config to json file

--- build-osx.sh
+++ build-osx.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
+# REQUIREMENTS
+# jdk-11.0.17_macos-x64_bin.dmg
+# npm install Adam-/create-dmg
+# brew install gradle
+
 set -e
 
-JDK_VER="11.0.4"
-JDK_BUILD="11"
-PACKR_VERSION="runelite-1.0"
+JDK_VER="17.0.5"
+JDK_BUILD="8"
+PACKR_VERSION="runelite-1.4"
 
 SIGNING_IDENTITY="Developer ID Application"
-ALTOOL_USER="user@icloud.com"
-ALTOOL_PASS="@keychain:altool-password"
 
 if ! [ -f OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz ] ; then
     curl -Lo OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz \
-        https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-${JDK_VER}%2B${JDK_BUILD}/OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
+        https://github.com/adoptium/temurin17-binaries/releases/download/jdk-${JDK_VER}%2B${JDK_BUILD}/OpenJDK17U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
 fi
 
-rm -f packr.jar
-curl -o packr.jar https://libgdx.badlogicgames.com/ci/packr/packr.jar
-
-echo "1647fded28d25e562811f7bce2092eb9c21d30608843b04250c023b40604ff26  OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz" | shasum -c
+echo "d7da2b16f23371d924ca241f55baadcd1ed9f864  OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz" | shasum -c
 
 # packr requires a "jdk" and pulls the jre from it - so we have to place it inside
 # the jdk folder at jre/
+
+
 if ! [ -d osx-jdk ] ; then
     tar zxf OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
     mkdir osx-jdk
-    mv jdk-11.0.4+11-jre osx-jdk/jre
+    mv jdk-${JDK_VER}+${JDK_BUILD}-jre osx-jdk/jre
 
     # Move JRE out of Contents/Home/
     pushd osx-jdk/jre
@@ -38,30 +40,11 @@ if ! [ -f packr_${PACKR_VERSION}.jar ] ; then
         https://github.com/runelite/packr/releases/download/${PACKR_VERSION}/packr.jar
 fi
 
-echo "18b7cbaab4c3f9ea556f621ca42fbd0dc745a4d11e2a08f496e2c3196580cd53  packr_${PACKR_VERSION}.jar" | shasum -c
+echo "f51577b005a51331b822a18122ce08fca58cf6fee91f071d5a16354815bbe1e3  packr_${PACKR_VERSION}.jar" | shasum -c
 
-java -jar packr_${PACKR_VERSION}.jar \
-    --platform \
-    mac \
-    --icon \
-    packr/darkan.icns \
-    --jdk \
-    osx-jdk \
-    --executable \
-    Darkan \
-    --classpath \
-    build/libs/darkan-shaded.jar \
-    --mainclass \
-    com.darkan.Loader \
-    --vmargs \
-    Xmx1024m \
-    Xss2m \
-    XX:CompileThreshold=1500 \
-    Djna.nosys=true \
-    --output \
-    native-osx/Darkan.app
+java -jar packr_${PACKR_VERSION}.jar packr/macOS-config.json
 
-cp build/filtered-resources/Info.plist native-osx/Darkan.app/Contents
+cp packr/Info.plist native-osx/Darkan.app/Contents
 
 echo Setting world execute permissions on Darkan
 pushd native-osx/Darkan.app
@@ -70,9 +53,11 @@ popd
 
 codesign -f -s "${SIGNING_IDENTITY}" --entitlements osx/signing.entitlements --options runtime native-osx/Darkan.app || true
 
+
 # create-dmg exits with an error code due to no code signing, but is still okay
 # note we use Adam-/create-dmg as upstream does not support UDBZ
-create-dmg --format UDBZ native-osx/Darkan.app.app native-osx/ || true
+# npm install --global Adam-/create-dmg
+create-dmg --volname Darkan --format UDBZ native-osx/Darkan.app native-osx/ || true
 
 mv native-osx/Darkan\ *.dmg native-osx/Darkan.dmg
 
